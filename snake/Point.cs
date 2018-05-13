33058a754cce8fc94e2a86e7283fca99712e2fcc
Safe_Mode <safe2mode@gmail.com>
2018-05-13 16:45:13 +0300

    Научил змейку есть и расти

--- snake/Point.cs
+++ snake/Point.cs
@@ -50,10 +50,15 @@ namespace snake
             }
         }
 
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
+
         public void Draw()
         {
-            Console.SetCursorPosition( x, y );
-            Console.Write( sym );
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
 
         public void Clear()
