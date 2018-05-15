fa31c73d4b4fb65c1be492a8bb81056976ffb209
Safe_Mode <safe2mode@gmail.com>
2018-05-15 18:58:16 +0300

    Прекратил игру при столкновении с препятствием, новый класс для рамки

---
+++ snake/Walls.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+	class Walls
+	{
+		List<Figure> wallList;
+
+		public Walls(int mapWidth, int mapHeight)
+		{
+			wallList = new List<Figure>();
+
+			//Отрисовка рамки
+			HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+			HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+			VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+
+			wallList.Add(upLine);
+			wallList.Add(downLine);
+			wallList.Add(leftLine);
+			wallList.Add(rightLine);
+		}
+
+		internal bool IsHit(Figure figure)
+		{
+			foreach (var wall in wallList)
+			{
+				if (wall.IsHit(figure))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public void Draw()
+		{
+			foreach (var wall in wallList)
+			{
+				wall.Draw();
+			}
+		}
+	}
+}
